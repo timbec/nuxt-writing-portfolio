266728781738b324d348cbdbdead71f21e807d63
Tim Beckett <timbeckett@Tims-Mac-mini.home>
2022-07-05 16:21:45 -0400

    basic footer updates.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>
+  <div class="container">
     <app-header />
     <nuxt />
+    <app-footer />
   </div>
 </template>
 
 <script>
 import AppHeader from "@/components/Header";
+import AppFooter from "@/components/Footer";
 export default {
   components: {
-    AppHeader
+    AppHeader,
+    AppFooter
   }
 };
 </script>
