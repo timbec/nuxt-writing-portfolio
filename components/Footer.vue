9ebb3b79ab428548f81e7682a98c1e20cfcf3b02
Tim Beckett <timbeckett@Tims-Mac-mini.home>
2022-07-05 16:34:14 -0400

    added about page content and basic footer.

---
+++ components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <footer class="site-footer">
+        <p class="copyright">
+            &#169; 2022 Tim Beckett
+        </p>
+    </footer>
+</template>
+
+<style scoped>
+.site-footer {
+    background: #09021d;
+    color: #fff;
+    width: 100%;
+    height: auto;
+    min-height: 20rem;
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-end;
+    margin: 0;
+}
+
+.copyright {
+    color: white;
+    margin: 4rem;
+}
+</style>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,8 +1,25 @@
 <template>
-    <section>
-        <h1>About page</h1>
+    <div class="about-container">
+        <h2 class="about__content--title">About</h2>
+        <section class="about">
+            <figure class="about__image">
+                <img src="@/assets/about/self-portrait.jpg" alt="Demon at Coney Island">
+            </figure>
 
-        <p>About me</p>
+            <article class="about__content">
+                <div class="about__content--text">
+                    <p>I write mostly fiction, some journalism, a couple of essays. I've been published in Granta, the
+                        Guardian, Urban Graffiti, Sensitive Skin Magazine and a few other places.</p>
+                    <p> After taking a sabbatical
+                        from writing, I'm back at it. What else am I going to do? I also write code and am building this
+                        site as I go. </p>
+                </div>
 
-    </section>
+            </article>
+
+
+
+        </section>
+
+    </div>
 </template>
