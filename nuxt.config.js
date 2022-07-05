4c13e3187d0bf3887c7896d0526b77f57032ef3d
Tim Beckett <timbeckett@Tims-Mac-mini.home>
2022-07-05 12:34:53 -0400

    adjusted nuxt.confif.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,6 @@
-export default {
+const pkg = require('./package');
+
+module.exports = {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: true,
 
@@ -37,8 +39,8 @@ export default {
     },
 
     transition: {
-      name: 'page',
-      mode: 'fade'
+      name: 'fade',
+      mode: 'out-in'
     }
   },
 
